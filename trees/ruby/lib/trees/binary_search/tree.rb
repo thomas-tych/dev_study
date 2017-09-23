a0ca5d7eec54b3e7ce27d6d7b4b41bb8f3abf0fd
Thomas Tych <thomas.tych@gmail.com>
2017-09-23 23:08:49 +0200

    [trees][ruby][binary_search] implement enumerator
    implemented with iterativ method :
    - each_pre : prefix-order enumerator
    - each_inf : infix-order enumerator
    - each_post : postfix-order enumerator

--- trees/ruby/lib/trees/binary_search/tree.rb
+++ trees/ruby/lib/trees/binary_search/tree.rb
@@ -1,3 +1,6 @@
+require 'pp'
+require 'byebug'
+
 module Trees
   module BinarySearch
     # reponsible for operations on BinarySearch Tree
@@ -8,12 +11,85 @@ module Trees
         @root = root
       end
 
-      def add(value)
-        if root
-          _insert(value)
-        else
-          @root = _new_node(value)
+      def add_iterative(value)
+        new_node = _new_node(value)
+        return _add_loop(new_node) if root
+        @root = new_node
+      end
 
+      alias add add_iterative
+
+      def find(value)
+        cur = root
+        while cur
+          comparison = cur <=> value
+          return cur if comparison.zero?
+          cur = comparison > 0 ? cur.left : cur.right
+        end
+      end
+
+      # cur / left / right
+      def each_pre
+        stack = [root].compact
+        while (cur = stack.pop)
+          stack.push(cur.right) if cur.right
+          stack.push(cur.left) if cur.left
+          yield cur.value
+        end
+      end
+
+      # left / cur / right
+      def each_inf
+        stack = []
+        cur = root
+        to_left = true
+        while cur
+          if to_left
+            while cur.left
+              stack.push cur
+              cur = cur.left
+            end
+          end
+
+          yield cur.value
+
+          if cur.right
+            cur = cur.right
+            to_left = true
+          else
+            cur = stack.pop
+            to_left = false
+          end
+        end
+      end
+
+      alias each each_inf
+
+      # left / right / cur
+      def each_post
+        stack = []
+        cur = root
+        from = nil
+
+        while cur
+          while (cur.left || cur.right)
+            if from
+              break if from == :right
+              break if from == :left && !cur.right
+              stack.push cur
+              cur = from == :left ? cur.right : cur.left
+            else
+              stack.push cur
+              cur = cur.left || cur.right
+            end
+            from = nil
+          end
+
+          yield cur.value
+
+          prev, cur = cur, stack.pop
+          break unless cur
+          from = cur.left && cur.left.equal?(prev) ? :left : :right
         end
       end
 
@@ -23,23 +99,15 @@ module Trees
         Node.new(value)
       end
 
-      def _insert(value)
-        cur = root
-        while cur
-          if value < cur.value
-            if cur.left
-              cur = cur.left
-            else
-              cur.left = _new_node(value)
-              cur = nil
-            end
+      def _add_loop(node)
+        cur_next = root
+        while (cur = cur_next)
+          if node < cur
+            cur_next = cur.left
+            cur.left = node unless cur_next
           else
-            if cur.right
-              cur = cur.right
-            else
-              cur.right = _new_node(value)
-              cur = nil
-            end
+            cur_next = cur.right
+            cur.right = node unless cur_next
           end
         end
       end
--- trees/ruby/test/trees/binary_search/tree_test.rb
+++ trees/ruby/test/trees/binary_search/tree_test.rb
@@ -1,6 +1,47 @@
 require 'test_helper'
 
 class TreeTest < Minitest::Test
+  def test_that_add_iterative_creates_the_root_node_on_empty_tree
+    tree = Trees::BinarySearch::Tree.new
+    new_value = Faker::Number.number(5).to_i
+
+    tree.add_iterative(new_value)
+    assert_equal(new_value, tree.root.value)
+  end
+
+  def test_that_add_iterative_append_to_left_for_value_less_than_root_value
+    root_value = Faker::Number.number(5).to_i
+    root = Trees::BinarySearch::Node.new(root_value)
+    tree = Trees::BinarySearch::Tree.new(root)
+
+    new_value = root_value - 1
+
+    tree.add_iterative(new_value)
+    assert_equal(new_value, tree.root.left.value)
+  end
+
+  def test_that_add_iterative_append_to_right_for_value_greater_than_root_value
+    root_value = Faker::Number.number(5).to_i
+    root = Trees::BinarySearch::Node.new(root_value)
+    tree = Trees::BinarySearch::Tree.new(root)
+
+    new_value = root_value + 1
+
+    tree.add_iterative(new_value)
+    assert_equal(new_value, tree.root.right.value)
+  end
+
+  def test_that_add_iterative_append_to_right_for_value_equal_to_root_value
+    root_value = Faker::Number.number(5).to_i
+    root = Trees::BinarySearch::Node.new(root_value)
+    tree = Trees::BinarySearch::Tree.new(root)
+
+    new_value = root_value
+
+    tree.add_iterative(new_value)
+    assert_equal(new_value, tree.root.right.value)
+  end
+
   def test_that_add_creates_the_root_node_on_empty_tree
     tree = Trees::BinarySearch::Tree.new
     new_value = Faker::Number.number(5).to_i
@@ -9,7 +50,7 @@ class TreeTest < Minitest::Test
     assert_equal(new_value, tree.root.value)
   end
 
-  def test_that_add_append_to_left_when_new_value_is_less_than_root_value
+  def test_that_add_append_to_left_for_value_less_than_root_value
     root_value = Faker::Number.number(5).to_i
     root = Trees::BinarySearch::Node.new(root_value)
     tree = Trees::BinarySearch::Tree.new(root)
@@ -20,7 +61,7 @@ class TreeTest < Minitest::Test
     assert_equal(new_value, tree.root.left.value)
   end
 
-  def test_that_add_append_to_right_when_new_value_is_greater_than_root_value
+  def test_that_add_append_to_right_for_value_greater_than_root_value
     root_value = Faker::Number.number(5).to_i
     root = Trees::BinarySearch::Node.new(root_value)
     tree = Trees::BinarySearch::Tree.new(root)
@@ -31,7 +72,7 @@ class TreeTest < Minitest::Test
     assert_equal(new_value, tree.root.right.value)
   end
 
-  def test_that_add_append_to_right_when_new_value_is_equal_to_root_value
+  def test_that_add_append_to_right_for_value_equal_to_root_value
     root_value = Faker::Number.number(5).to_i
     root = Trees::BinarySearch::Node.new(root_value)
     tree = Trees::BinarySearch::Tree.new(root)
@@ -40,5 +81,62 @@ class TreeTest < Minitest::Test
 
     tree.add(new_value)
     assert_equal(new_value, tree.root.right.value)
+  end
+
+  def test_that_find_return_truthy_when_value_is_found
+    tree = Trees::BinarySearch::Tree.new
+    values = Array.new(10) { rand(100) }
+    values.each { |value| tree.add(value) }
+
+    random_value = values.sample
+    assert(tree.find(random_value))
+  end
+
+  def test_that_find_return_falsy_when_value_is_not_found
+    tree = Trees::BinarySearch::Tree.new
+    values = Array.new(10) { rand(100) }
+    values.each { |value| tree.add(value) }
+
+    random_value = 0
+    loop do
+      random_value = rand(100)
+      break unless values.include? random_value
+    end
+
+    refute(tree.find(random_value))
+  end
+
+  def test_that_each_pre_iterate_using_pre_order
+    tree = Trees::BinarySearch::Tree.new
+    values = [25, 83, 78, 37, 82, 44, 21, 73, 56, 55]
+    values.each { |value| tree.add(value) }
+
+    expected_pre_order_values = [25, 21, 83, 78, 37, 44, 73, 56, 55, 82]
+    pre_order_values = []
+    tree.each_pre { |value| pre_order_values << value }
+    assert_equal(expected_pre_order_values, pre_order_values)
+  end
+
+  def test_that_each_pre_iterate_using_inf_order
+    tree = Trees::BinarySearch::Tree.new
+    values = Array.new(10) { rand(100) }
+    values.each { |value| tree.add(value) }
+
+    expected_inf_order_values = values.sort
+    inf_order_values = []
+    tree.each_inf { |value| inf_order_values << value }
+    assert_equal(expected_inf_order_values, inf_order_values)
+  end
+
+  def test_that_each_pre_iterate_using_post_order
+    tree = Trees::BinarySearch::Tree.new
+    values = [25, 21, 83, 78, 37, 44, 73, 56, 55, 82, 15, 23, 12, 17, 11, 16]
+    values.each { |value| tree.add(value) }
+
+    expected_post_order_values = [11, 12, 16, 17, 15, 23, 21, 55, 56, 73, 44,
+                                  37, 82, 78, 83, 25]
+    post_order_values = []
+    tree.each_post { |value| post_order_values << value }
+    assert_equal(expected_post_order_values, post_order_values)
   end
 end
